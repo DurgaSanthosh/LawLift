c5c1bb7ef4c65fb4d398f15f42369e3b8359227a
voyager153 <durgasanthosh123@gmail.com>
2023-02-24 13:42:34 +0530

    auth added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Routes, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Protected from './components/Protected';
+import { AuthContextProvider } from './context/AuthContext';
+import Signout from './pages/Signout';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthContextProvider>
+        
+        <Routes>
+        <Navbar />
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route
+            path='/account'
+            element={
+              <Protected>
+                <Signout />
+              </Protected>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxxNDFvbEvbT-xxE2H5JpZDTE1d7e_roA",
+  authDomain: "lawlift-862a0.firebaseapp.com",
+  projectId: "lawlift-862a0",
+  storageBucket: "lawlift-862a0.appspot.com",
+  messagingSenderId: "715823159715",
+  appId: "1:715823159715:web:09454e2b6f8de656d6e216",
+  measurementId: "G-NQGEFFVZMM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
